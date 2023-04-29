8f08278e302c4b4221892d23860c4d28e3bd7d69
wanderlust-li <@wanderlust-li>
2023-04-29 19:02:54 +0300

    CreateDb

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
