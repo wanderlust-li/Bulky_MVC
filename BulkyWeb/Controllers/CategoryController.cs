8229a85f4dd1be88c9bb1d0c1683df6de7ebdd1f
wanderlust-li <@wanderlust-li>
2023-04-30 14:27:27 +0300

    Category List

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BulkyWeb.Data;
+using BulkyWeb.Models;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db; 
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     // GET
     public IActionResult Index()
     {
-        return View();
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
     }
 }
