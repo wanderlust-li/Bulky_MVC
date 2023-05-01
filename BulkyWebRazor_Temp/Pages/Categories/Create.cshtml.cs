bdbe74880f29cafedc3bb61ccb8db53cc7c75425
wanderlust-li <@wanderlust-li>
2023-05-01 20:55:59 +0300

    Toastr Notifications

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    
+    public class Create : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public Create(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories;
+
+public class Edit : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    [BindProperty]
+    public Category Category { get; set; }
+
+    public Edit(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int? id)
+    {
+        if (id != null && id != 0)
+            Category = _db.Categories.Find(id);
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToPage("Index");
+        }
+
+        return Page();
+    }
+}
