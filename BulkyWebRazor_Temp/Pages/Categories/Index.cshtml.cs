b51bc2081ef9bcb43971eb9a9996df446328b470
wanderlust-li <@wanderlust-li>
2023-05-01 20:55:59 +0300

    Bulky Models, Db, Ulility

---
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWebRazor_Temp.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BulkyWebRazor_Temp.Models;
+
+namespace BulkyWebRazor_Temp.Pages.Categories;
+
+public class Index : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public List<Category> CategoryList { get; set; }
+    public Index(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet()
+    {
+        CategoryList = _db.Categories.ToList();
+        
+    }
+}
