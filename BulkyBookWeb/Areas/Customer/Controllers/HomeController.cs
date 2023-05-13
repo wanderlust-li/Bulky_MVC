31eafb7b993b6c124d58e6a22f4b10203bf3c2a5
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-13 11:47:03 +0300

    Update Home Page

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using BulkyBook.DataAcess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using BulkyBook.Models;
 
@@ -7,15 +8,18 @@ namespace BulkyBookWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includePropetries: "Category");
+        return View(productList);
     }
 
     public IActionResult Privacy()
