952805173bae3a9bb9e73bbee9c86c0cfbc51983
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-13 19:58:12 +0300

    roles on registration

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 
 builder.Services.AddRazorPages();
