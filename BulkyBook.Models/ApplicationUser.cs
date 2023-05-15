34c0f50172b6cba5cbe6656e9de6f2e361fce9d2
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-15 21:11:19 +0300

    display company dropdown

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace BulkyBook.Models;
 
@@ -12,4 +14,9 @@ public class ApplicationUser : IdentityUser
     public string? City { get; set; }
     public string? State { get; set; }
     public string? PostalCode { get; set; }
+    
+    public int? CompanyId { get; set; }
+    [ForeignKey("CompanyId")]
+    [ValidateNever]
+    public Company Company { get; set; }
 }
