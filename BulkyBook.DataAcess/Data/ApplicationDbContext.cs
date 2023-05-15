fe55825aeeb6f9aef66fbdb5c7976ada392aa26c
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-15 21:01:32 +0300

    update company upsert

--- BulkyBook.DataAcess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAcess/Data/ApplicationDbContext.cs
@@ -5,15 +5,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBook.DataAcess.Data
 {
-
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext()
         {
         }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Bulky;Trusted_Connection=True;");
@@ -22,23 +23,32 @@ namespace BulkyBook.DataAcess.Data
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Product> Products { get; set; } = null!;
         public DbSet<Company> Companies { get; set; } = null!;
-        
+
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
             );
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company
+                {
+                    Id = 1, Name = "Tech Solution", StreetAddress = "123 Tech St", City = "Tech City",
+                    PostalCode = "12121",
+                    State = "Il", PhoneNumber = "314315135"
+                }
+            );
             modelBuilder.Entity<Product>().HasData(
                 new Product
                 {
-                    Id = 1, 
-                    Title = "Fortune of Time", 
+                    Id = 1,
+                    Title = "Fortune of Time",
                     Author = "Billy Spark",
                     Description = "Praesent vitae sodales libero.",
                     ISBN = "SWD999901",
---
+++ BulkyBook.DataAcess/Migrations/20230515175950_addCompanyRecords.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanyRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[] { 1, "Tech City", "Tech Solution", "314315135", "12121", "Il", "123 Tech St" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
