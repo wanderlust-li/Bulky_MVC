b3f56ba0e2bd5efc979b4fbb8d89fb299873c903
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-16 13:41:06 +0300

    register company user

--- BulkyBookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyBookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -172,6 +172,11 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                 user.State = Input.State;
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if (Input.Role == SD.Role_Comp)
+                    user.CompanyId = Input.CompanyId;
+                
+                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
