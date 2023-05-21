0e3bef436748395ef96c06db90efd72d315a01a7
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-21 14:42:21 +0300

    update cart controller

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers;
+
 [Area("Customer")]
 [Authorize]
 public class CartController : Controller
@@ -16,6 +17,7 @@ public class CartController : Controller
     {
         _unitOfWork = unitOfWork;
     }
+
     // GET
     public IActionResult Index()
     {
@@ -33,8 +35,43 @@ public class CartController : Controller
             cart.Price = GetPriceBasedOnQuntity(cart);
             ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
         }
-        
+
         return View(ShoppingCartVM);
+    }
+
+    public IActionResult Plus(int cartId)
+    {
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+        cartFromDb.Count += 1;
+        _unitOfWork.ShoppingCart.Update(cartFromDb);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Minus(int cartId)
+    {
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+        if (cartFromDb.Count <= 1)
+        {
+            // remove that from cart
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+        }
+        else
+        {
+            cartFromDb.Count -= 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+        }
+
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Remove(int cartId)
+    {
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+        _unitOfWork.ShoppingCart.Remove(cartFromDb);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
     }
 
     private double GetPriceBasedOnQuntity(ShoppingCart shoppingCart)
